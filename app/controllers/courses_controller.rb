139954284f4eae71ada8b8e60d43b5d4e0f5b2f2
apuri-uc <apuri1@chicagobooth.edu>
2024-07-23 16:41:18 +0000

    updatie

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,53 +1,56 @@
 class CoursesController < ApplicationController
-  def index
-    @courses = Course.all.order({ :created_at => :desc })
+  def update
+    id = params.fetch("path_id")
+    matching_records = Course.where({ :id => the_id }).at(0)
+    the_course = matching_records.at(0)
+    the_course.title = params.fetch("query_title")
+    the_course.term_offered = params.fetch("query_term_offered")
+    the_course.course_id = params.fetch("query_path_id")
 
-    render({ :template => "courses/index" })
-  end
-
-  def show
-    the_id = params.fetch("path_id")
-    @course = Course.where({:id => the_id }).at(0)
-
-    render({ :template => "courses/show" })
+    if the_course.valid?
+      the_course.save
+      redirect_to("/courses/#{@the_course.id}", { :notice => "Course updated successfully."} )
+    else
+      redirect_to("/courses/#{@the_course.id}", { :alert => "Course failed to update successfully." })
+    end
   end
 
   def create
-    @course = Course.new
-    @course.title = params.fetch("query_title")
-    @course.term_offered = params.fetch("query_term_")
-    @course.department_id = params.fetch("query_department_id")
+    c = Course.new
+    c.title = params.fetch("query_title")
+    c.term_offered = params.fetch("query_term_offered")
+    c.course_id = params.fetch("query_path_id")
 
-    if @course.valid?
-      @course.save
+    if c.valid?
+      c.save
       redirect_to("/courses", { :notice => "Course created successfully." })
     else
       redirect_to("/courses", { :notice => "Course failed to create successfully." })
     end
   end
 
-  def update
-    id = params.fetch("path_id")
-    @course = Course.where({ :id => the_id }).at(0)
-
-    @course.title = params.fetch("query_title")
-    @course.term_offered = params.fetch("query_term_offered")
-    @course.department_id = params.fetch("query_department_id")
-
-    if @course.valid?
-      @course.save
-      redirect_to("/courses/#{@course.id}", { :notice => "Course updated successfully."} )
-    else
-      redirect_to("/courses/#{@course.id}", { :alert => "Course failed to update successfully." })
-    end
-  end
-
   def destroy
-    the_id = params.fetch("path")
-    @course = Course.where({ :id => the_id }).at(0)
-
-    @course.destroy
+    the_id = params.fetch("path_id")
+    matching_records = Course.where({ :id => the_id })
+    the_course = matching_records.at(0)
+    the_course.destroy
 
     redirect_to("/courses", { :notice => "Course deleted successfully."} )
+  end
+
+  def index
+    matching_courses = Course.all
+    @list_of_courses = matching_courses.order({ :created_at => :desc })
+
+    render({ :template => "courses/index" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+    
+    matching_courses = Course.where({:id => the_id })
+    @the_course = matching_courses.at(0)
+
+    render({ :template => "courses/show" })
   end
 end
--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,29 +1,4 @@
 class DepartmentsController < ApplicationController
-  def index
-    @departments = Department.all.order({ :created_at => :desc })
-
-    render({ :template => "departments/index" })
-  end
-
-  def show
-    the_id = params.fetch("path_id")
-    @department = Department.where({:id => the_id })
-
-    render({ :template => "departments/show" })
-  end
-
-  def create
-    @department = Department.new
-    @department.name = params.fetch("query_name")
-
-    if @department.valid?
-      @department.save
-      redirect_to("/departments", { :notice => "Department created successfully." })
-    else
-      redirect_to("/departments", { :notice => "Department failed to create successfully." })
-    end
-  end
-
   def update
     the_id = params.fetch("path_id")
     @department = Department.where({ :id => the_id }).at(0)
@@ -38,6 +13,18 @@ class DepartmentsController < ApplicationController
     end
   end
 
+  def create
+    @department = Department.new
+    @department.name = params.fetch("query_name")
+
+    if @department.valid?
+      @department.save
+      redirect_to("/departments", { :notice => "Department created successfully." })
+    else
+      redirect_to("/departments", { :notice => "Department failed to create successfully." })
+    end
+  end
+
   def destroy
     the_id = params.fetch("path_id")
     @department = Department.where({ :id => the_id }).at(0)
@@ -45,5 +32,19 @@ class DepartmentsController < ApplicationController
     @department.destroy
 
     redirect_to("/departments", { :notice => "Department deleted successfully."} )
+  end
+
+  def index
+    @departments = Department.all.order({ :created_at => :desc })
+
+    render({ :template => "departments/index" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+    matching_departments = Department.where({:id => the_id })
+    @the_department = matching_departments.at(0)
+
+    render({ :template => "departments/show" })
   end
 end
